e14cab1ff738aa834d9dbd68b377ecf5e40af66a
CinthiaRigoni <c.rigonig@gmail.com>
2023-07-22 17:08:08 -0300

    add funcion suma

---
+++ Test_calculadora.py
@@ -0,0 +1,10 @@
+from calculadora import *
+import pytest
+
+@pytest.mark.parametrize("a, b, res",[
+    (2,4,6),
+    (-4,6,2),
+    (-5,-1,-6),
+])
+def test_suma(a, b, res):
+    assert suma(a, b) == res
---
+++ calculadora.py
@@ -0,0 +1,7 @@
+# Funciones
+
+'''Función que recibe dos números como parámetros y
+devuelve la suma de los mismos'''
+
+def suma(a, b):
+    return (a+b)
